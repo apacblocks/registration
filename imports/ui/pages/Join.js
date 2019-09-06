2ecbea63dee64841592a53fbc7089e9f04fcacda
louloudev <louislui95111@gmail.com>
2019-09-06 15:50:54 +0800

    Problem: no Bio
    Solution: add a Bio section when register

--- imports/ui/pages/Join.js
+++ imports/ui/pages/Join.js
@@ -10,6 +10,7 @@ import Container from '@material-ui/core/Container';
 import QrReader from 'react-qr-reader'
 import ArrowBack from '@material-ui/icons/ArrowBack'
 import IconButton from '@material-ui/core/IconButton'
+import { Meteor } from 'meteor/meteor';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -43,9 +44,9 @@ export default function Join(props) {
   }
 
 
-
-  return (
-    <React.Fragment>
+  if (Meteor.user()) {
+    return(
+      <React.Fragment>
       <CssBaseline />
       <AppBar>
         <Toolbar>
@@ -64,17 +65,46 @@ export default function Join(props) {
       <Toolbar id="back-to-top-anchor" />
       <Container style={{ minHeight: '100vh' }}>
         <Box my={2}>
-          <h2>Please scan your sponsor's QR code</h2>
-          <p>You must be physically in attendance at an APAC Blocks event to become a member and you must be sponsored by an existing member.</p>
-          <p>You will be asked to allow this site to access your camera. If you are having problems, please try another browser.</p>
-          <QrReader
-            delay={300}
-            onError={this.handleError}
-            onScan={this.handleScan}
-            style={{ maxWidth: 500, margin: '0 auto' }}
-          />
+          <h2>You are currently logged in as {Meteor.user().profile.realName}</h2>
+          <p>Please log out to sign up a new account.</p>
         </Box>
       </Container>
     </React.Fragment>
-  );
+    )
+  }
+  else {
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <AppBar>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="go back"
+              onClick={() => window.history.back()}
+            >
+              <ArrowBack />
+            </IconButton>
+            <Typography variant="h6">APAC Blocks</Typography>
+          </Toolbar>
+        </AppBar>
+        <Toolbar id="back-to-top-anchor" />
+        <Container style={{ minHeight: '100vh' }}>
+          <Box my={2}>
+            <h2>Please scan your sponsor's QR code</h2>
+            <p>You must be physically in attendance at an APAC Blocks event to become a member and you must be sponsored by an existing member.</p>
+            <p>You will be asked to allow this site to access your camera. If you are having problems, please try another browser.</p>
+            <QrReader
+              delay={300}
+              onError={this.handleError}
+              onScan={this.handleScan}
+              style={{ maxWidth: 500, margin: '0 auto' }}
+            />
+          </Box>
+        </Container>
+      </React.Fragment>
+    );
+  }
 }
