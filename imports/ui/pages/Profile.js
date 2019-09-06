8fc04c21aa65a219cbe8e8329c4613e8df3118b7
louloudev <louislui95111@gmail.com>
2019-09-06 14:10:34 +0800

    Problem: not showing real name
    Solution: show real name

--- imports/ui/pages/Profile.js
+++ imports/ui/pages/Profile.js
@@ -99,7 +99,7 @@ export default function Login(props) {
                 </List>
                 <Divider />
                 <List className={classes.root}
-                subheader={<ListSubheader>User Name</ListSubheader>}>
+                subheader={<ListSubheader>Name</ListSubheader>}>
                     <Grid container justify="center" alignItems="center">
                         <ListItem>
                             <ListItemText id="switch-list-label-darkMode" primary={Meteor.user().profile.realName} />
